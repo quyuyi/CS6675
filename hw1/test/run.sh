523d7960ce2f0b11a84d0143ddeba6ce4943e42f
quyuyi <quyuyi@umich.edu>
2021-01-28 01:58:38 -0500

    hw1 init

---
+++ hw1/test/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# src: https://lucene.apache.org/core/8_7_0/demo/overview-summary.html#overview.description
+
+echo -n "Do you want to (re)build an index? Enter Y or n: "
+read x
+if [ "$x" = "Y" ]
+then
+    # Build an index
+    java -cp ./lucene-analyzers-common-8.7.0.jar:./lucene-core-8.7.0.jar:./lucene-demo-8.7.0.jar:./lucene-queryparser-8.7.0.jar org.apache.lucene.demo.IndexFiles -docs cc.gatech.edu
+fi
+
+# Search the index
+java -cp ./lucene-analyzers-common-8.7.0.jar:./lucene-core-8.7.0.jar:./lucene-demo-8.7.0.jar:./lucene-queryparser-8.7.0.jar org.apache.lucene.demo.SearchFiles
